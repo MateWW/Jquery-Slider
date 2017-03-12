02f2e32f5d30d4e67278e0a1f31823c5bba6ee85
MatEw <maateew@gmail.com>
2017-03-12 23:22:23 +0100

    Poprawione znikanie elementu

--- slider.jquery.js
+++ slider.jquery.js
@@ -8,7 +8,6 @@
 		var that= this,
 			config=$.fn.MWslider.getConfig(config);
 
-
 		$.fn.MWslider.setHeight(that);
 		$(window).on("resize",function(){$.fn.MWslider.setHeight(that);});
 
@@ -83,36 +82,29 @@
 
 	$.fn.MWslider.checkPosition = function(cent,len){
 			var left=(cent-1)<0 ? len-1 : cent-1,
-			right=(cent+2)>len ? 0 : cent+1;
-			return [left,cent,right];
+			right=(cent+2)>len ? 0 : cent+1,
+			prevleft=(left-1)<0 ? len-1 : left-1,
+			prevright=(right+2)>len ? 0 : right+1
+			return [left,cent,right,prevleft,prevright];
 	}
 
-	$.fn.MWslider.moveSlider = function(that,site,config){
+	$.fn.MWslider.moveSlider = function(that,side,config){
 
 		var len=that.elements.length;
 
-		if(site==="right")
-		{	
-			that.center= that.center+2>len ? 0 : that.center+1;
-		}		
-		if(site==="left")
-		{
+		if(side==="right")
+			that.center= that.center+2>len ? 0 : that.center+1;		
+		else if(side==="left")
 			that.center= that.center-1<0 ? len-1 : that.center-1;
-		}
+
 
 		var pos=$.fn.MWslider.checkPosition(that.center,len);
-			
-		elements={
-			left:that.elements.eq(pos[0]),
-			right:that.elements.eq(pos[2]),
-			center:that.elements.eq(pos[1])
-		}		
-		
+				
 		that.elements.removeClass();
-
-		elements.right.addClass("right");				
-		elements.center.addClass("center");
-		elements.left.addClass("left");
+		
+		that.elements.eq(pos[2]).addClass("right");				
+		that.elements.eq(pos[1]).addClass("center");
+		that.elements.eq(pos[0]).addClass("left");
 
 	}
 
